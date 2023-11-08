14fb5391fbf9e90b16cd80db4d240a19c46d1de0
shinohara <emsmockn@gmail.com>
2023-11-08 22:29:44 +0900

    電源管理ボード用ソフトを登録

---
+++ IoT_Power_Cnt/Core/Inc/app_cmd.h
@@ -0,0 +1,13 @@
+/*
+ * app_cmd.h
+ *
+ *  Created on: Nov 3, 2023
+ *      Author: s20-0
+ */
+
+#ifndef INC_APP_CMD_H_
+#define INC_APP_CMD_H_
+
+#include "main.h"
+
+#endif /* INC_APP_CMD_H_ */
---
+++ IoT_Power_Cnt/Core/Src/app_cmd.c
@@ -0,0 +1,88 @@
+/*
+ * app_cmd.c
+ *
+ *  Created on: Nov 3, 2023
+ *      Author: s20-0
+ */
+
+#include "app_cmd.h"
+
+void send_sysCpcReset_cmd(void);
+
+
+void app_syscmd_run(uint8_t *data,uint16_t len)
+{
+	uint16_t cmd;
+	cmd = data[3];
+
+	switch(cmd)
+	{
+	case SYS_MEM_READ:
+		break;
+	case SYS_MEM_WRITE:
+		break;
+	case SYS_CPU_RESET:
+		send_sysCpcReset_cmd();
+		break;
+	case SYS_SET_RTC:
+		break;
+	case SYS_GET_TIME:	// DataGet all
+		break;
+	case SYS_GET_FW_VER:	// GyroDataGet
+		break;
+	case SYS_SET_DB:	// AcceleDataGet
+		break;
+	case SYS_GET_DB:	// MagDataGet
+		break;
+	case SYS_SET_MODE:	// MagDataGet
+		break;
+	case SYS_GET_MODE:	// MagDataGet
+		break;
+	default:
+		break;
+	}
+
+}
+
+
+
+void app_pwrcmd_run(uint8_t *data,uint16_t len)
+{
+	uint8_t cmd;
+	cmd = data[3];
+
+	switch(cmd)
+	{
+	case PWR_CHECK_VOLT:
+        drv_pc_cmd_tx_sync(PWR_CHECK_VOLT,CMD_IND,(uint8_t*) adcData,6);
+		break;
+	case PWR_CHECK_PORT:
+		break;
+	case PWR_SET_PORT:
+		break;
+	default:
+		break;
+    }
+
+}
+
+void app_per_wet_cmd(uint8_t *data)
+{
+    if(data[0] == 0x00)
+    {
+        
+    }
+
+}
+
+
+void send_sysBoot_cmd(void)
+{
+	drv_pc_cmd_tx_sync(SYS_BOOT,CMD_IND,NULL,0);
+}
+
+
+void send_sysCpcReset_cmd(void)
+{
+	HAL_NVIC_SystemReset();
+}
